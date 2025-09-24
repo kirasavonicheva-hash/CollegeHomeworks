a33f7f641a84af567d9d73af9e9a8c23bbb2aecf
kirasavonicheva-hash <kirasavonicheva@gmail.com>
2025-09-24 14:00:24 +0300

    Add files via upload

---
+++ Module1Part3/Task4.py
@@ -0,0 +1,9 @@
+meters = float(input())
+centimeters = meters * 100
+decimeters = meters * 10
+milimeters = meters * 1000
+miles = meters * 0.000621371
+print ( f"{meters} = {centimeters}")
+print ( f"{meters} = {decimeters}")
+print ( f"{meters} = {milimeters}")
+print ( f"{meters} = {miles}")
---
+++ Module1Part3/task5.py
@@ -0,0 +1,4 @@
+number_int = int(input())
+number_str = str(number_int)
+reversed_str = number_str[::-1]
+print(int(reversed_str))
