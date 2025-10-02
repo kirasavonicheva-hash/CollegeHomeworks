95d844994c14a16c2696e240b18d7335ef84e548
kirasavonicheva-hash <kirasavonicheva@gmail.com>
2025-10-02 12:26:23 +0300

    Add files via upload

---
+++ Module1Part3/Modul2Task2Part2.py
@@ -0,0 +1,17 @@
+months = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+month_number = int(input())
+if 1 <= month_number <=  12:
+        print (months[month_number])
---
+++ Module1Part3/Modul2Task2Part4.py
@@ -0,0 +1,9 @@
+num1 = int(input())
+num2 = int(input())
+
+if num1 == num2:
+ print("числ равны")
+elif num1 > num2:
+ print(num2 , num1)
+else:
+ print(num1 , num2)
