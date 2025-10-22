5434a2aee14cfd548714bdb8c35c77ad53edd66f
kirasavonicheva-hash <kirasavonicheva@gmail.com>
2025-10-22 18:21:18 +0300

    Update Part1.py

--- Modul2/Part1.py
+++ Modul2/Part1.py
@@ -3,6 +3,7 @@ b=int(input())
 summ=0
 summch=0
 summ9=0
+cai=0
 for i in range(a,b+1):
     if i%2!=0:
         summ+=i
@@ -13,3 +14,7 @@ for i in range(a,b+1):
     if i%9==0:
         summ9+=i
         print("Сумма кратных на 9: ",summ9)
+    if i%2 !=0:
+        cai+=i
+        print("Средниеарифметическое",cai)
+
