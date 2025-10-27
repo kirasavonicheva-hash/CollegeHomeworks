88e4762af226ff8a962881401c8e751bda50a828
kirasavonicheva-hash <kirasavonicheva@gmail.com>
2025-10-27 15:19:56 +0300

    Update Part1.py

--- Modul2/Part1.py
+++ Modul2/Part1.py
@@ -1,23 +1,22 @@
 a = int(input())
 b = int(input())
-summ=0
-summch=0
-summ9=0
-cai=0
-for i in range(a,b+1):
-    if i%2!=0:
-        summ+=i
+sum = 0
+count = 0
+sumch = 0
+count1 = 0
+sumch9 = 0
+count2 = 0
 
-    if i%2==0:
-        summch+=i
-
-    if i%9==0:
-        summ9+=i
-
-    if i%3==0:
-        cai+=i
-
-print("Сумма нечетных: ",summ)
-print("Сумма четных: ",summch)
-print("Сумма кратных на 9: ",summ9)
-print("Средниеарифметичекое: ",cai)
+for i in range(a, b+1):
+    if i % 2 == 0:
+        sum += i
+        count += 1
+    if i % 2 != 0:
+        sumch += i
+        count1 += 1
+    if i % 9 == 0:
+        sumch9 += i
+        count2 += 1
+print("СРЕДНЕАРИФМ не четных", sumch/count1)
+print("Средние арифметическое четных", sum/count)
+print("Средниеарифметическое кратных на 9", sumch9/count2)
