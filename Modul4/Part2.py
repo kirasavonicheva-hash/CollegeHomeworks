9d2c69abae5441addd487d10d7344c6464f78d58
kirasavonicheva-hash <kirasavonicheva@gmail.com>
2025-11-17 15:33:11 +0300

    Delete Modul4 directory

--- Modul4/Part2.py
+++
@@ -1,6 +0,0 @@
-string = input("Введите строку: ")
-reversed_string = string[::-1]
-if string == reversed_string:
-   print("Строка является палиндромом")
-else:
-   print("Строка не является палиндромом")
--- Modul4/Part3.py
+++
@@ -1,5 +0,0 @@
-text = input()
-words = input().split()
-for word in words:
-    text = text.replace(word, word.upper())
-print(text)
